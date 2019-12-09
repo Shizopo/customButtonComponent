da51bd041c8ad483a6eb62800425fd859899e5d2
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2019-12-09 14:38:50 +0200

    basic setup

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React from "react";
+
+class CustomButton extends React.Component {
+  render() {
+    return <h1>Some fancy heading</h1>;
+  }
+}
+
+export default CustomButton;
