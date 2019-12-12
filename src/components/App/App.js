a176cfbb6be505e5b7ed53d6cb3d753d8328100e
Dmytro Dubinets <18703715+Shizopo@users.noreply.github.com>
2019-12-12 16:48:55 +0200

    add CustomButton component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,29 @@
+// @flow
+
 import React from "react";
-import logo from "../../logo.svg";
-import "./App.css";
+import "./App.sass";
 
 import CustomButton from "../CustomButton/";
 
 function App() {
+  const customStyle = {
+    backgroundColor: "#f5f5f5",
+    color: "#000",
+    padding: ".5em"
+  };
+
   return (
     <div className="App">
-      <CustomButton />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
+      <CustomButton
+        buttonValue="Button"
+        buttonSize="M"
+        buttonRole="primary"
+        // buttonState="hover"
+        // nonStyled
+        // isCustom={true}
+        // customStyle={customStyle}
+        // iconUrl="http://simpleicon.com/wp-content/uploads/cute.png"
+      />
     </div>
   );
 }
--- src/components/CustomButton/CustomButton.js
+++ src/components/CustomButton/CustomButton.js
@@ -1,10 +1,117 @@
 // @flow
 
 import React from "react";
+import "./CustomButton.sass";
 
-class CustomButton extends React.Component {
+type Props = {
+  buttonValue?: string,
+  buttonSize?: string,
+  buttonRole?: string,
+  buttonState?: string,
+  nonStyled?: boolean,
+  isCustom?: boolean,
+  customStyle?: {},
+  iconUrl?: string
+};
+
+type State = {};
+
+class CustomButton extends React.PureComponent<Props, State> {
+  addSizeModifier = () => {
+    let { buttonSize } = this.props;
+    buttonSize = buttonSize ? buttonSize.toLowerCase() : "m";
+
+    switch (buttonSize) {
+      case "s":
+        return "button--small";
+      case "m":
+        return "button--medium";
+      case "l":
+        return "button--large";
+      default:
+        return "button--medium";
+    }
+  };
+
+  addRoleModifier = () => {
+    let { buttonRole } = this.props;
+    let rawClassName = this.addSizeModifier();
+    buttonRole = buttonRole ? buttonRole.toLowerCase() : "primary";
+
+    switch (buttonRole) {
+      case "primary":
+        return rawClassName.concat("--primary");
+      case "secondary":
+        return rawClassName.concat("--secondary");
+      case "success":
+        return rawClassName.concat("--success");
+      case "warning":
+        return rawClassName.concat("--warning");
+      case "error":
+        return rawClassName.concat("--error");
+      default:
+        return rawClassName.concat("--primary");
+    }
+  };
+
+  addStateModifier = () => {
+    let { buttonState, nonStyled } = this.props;
+    let rawClassName = !nonStyled
+      ? this.addRoleModifier()
+      : "button--non-styled";
+    buttonState = buttonState ? buttonState.toLowerCase() : null;
+
+    switch (buttonState) {
+      case "hover":
+        return rawClassName.concat(" is-hover");
+      case "active":
+        return rawClassName.concat(" is-active");
+      case "disabled":
+        return rawClassName.concat(" is-disabled");
+      case "loading":
+        return rawClassName.concat(" is-loading");
+      default:
+        return rawClassName;
+    }
+  };
+
+  generateClassName = () => this.addStateModifier();
+
+  generateButton = () => {
+    const {
+      buttonValue = "Button",
+      isCustom,
+      customStyle,
+      iconUrl
+    } = this.props;
+
+    if (isCustom) {
+      return (
+        <div className="button--custom" style={customStyle}>
+          <span className="button__icon">
+            {iconUrl ? (
+              <img
+                src={iconUrl}
+                alt="Button icon"
+                className="button__icon__img"
+              ></img>
+            ) : null}
+          </span>
+          <p className="button__text">{buttonValue}</p>
+        </div>
+      );
+    }
+
+    return (
+      <div className={this.generateClassName()}>
+        <span className="button__icon"></span>
+        <p className="button__text">{buttonValue}</p>
+      </div>
+    );
+  };
+
   render() {
-    return <h1>Some fancy heading</h1>;
+    return this.generateButton();
   }
 }
 
